bbd4239b785db74282f8e46d43605272df521539
zlc1994 <zlc612@gmail.com>
2018-05-24 23:10:24 +0800

    change spider user-agent

--- novelspider/novelspider/settings.py
+++ novelspider/novelspider/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'novelspider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'novelspider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
